737efdd19f76bb01c240befe808f87a9f62fa8c3
Manu <mjvillar@iesoretania.es>
2024-10-03 12:32:15 +0200

    Actualizado a Octubre 2024

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "es.iesoretania.listviewpersonalizado"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "es.iesoretania.listviewpersonalizado"
@@ -34,10 +34,10 @@ android {
 
 dependencies {
 
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
 }
